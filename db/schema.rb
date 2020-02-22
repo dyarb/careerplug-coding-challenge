93e66e5363ea86b3863bb2dabc9cdfd9dd45b9fe
David Yarbro <y.4rbr0@gmail.com>
2020-02-22 11:54:33 -0600

    Add comments cache to posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_172935) do
+ActiveRecord::Schema.define(version: 2020_02_22_173857) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_172935) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count"
   end
 
 end
